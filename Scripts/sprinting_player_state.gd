10eaaadbc6e8972aa6c1662d53f66e4781465ac9
kemba <miles.welsh@live.com>
2025-07-14 14:18:46 -0500

    Adding sprinting_player_state.gd

---
+++ Scripts/sprinting_player_state.gd
@@ -0,0 +1,15 @@
+class_name SprintingPlayerState
+
+extends State
+
+@export var TopAnimSpeed : float = 1.6
+
+func enter() -> void:
+	Global.player._speed = Global.player.SPEED_SPRINTING
+
+func update(delta):
+	pass
+
+func _input(event) -> void:
+	if event.is_action_released("sprint"):
+		transition.emit("WalkingPlayerState")
