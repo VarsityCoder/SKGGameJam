15a97180094926993830ca3a6fe01b153c964fd5
kemba <miles.welsh@live.com>
2025-07-14 14:19:39 -0500

    Updating walking_player_state.gd

--- Scripts/walking_player_state.gd
+++ Scripts/walking_player_state.gd
@@ -2,7 +2,14 @@ class_name WalkingPlayerState
 
 extends State
 
+func enter() -> void:
+	Global.player._speed = Global.player.SPEED_DEFAULT
+
 func update(delta):
 	if Global.player.velocity.length() == 0.0:
 		transition.emit("IdlePlayerState")
+
+func _input(event) -> void:
+	if event.is_action_pressed("sprint") and Global.player.is_on_floor():
+		transition.emit("SprintingPlayerState")
 	
