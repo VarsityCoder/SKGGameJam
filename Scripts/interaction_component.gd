8068105c52986c35e994db95d228f50b4480c828
kemba <miles.welsh@live.com>
2025-07-17 00:53:12 -0500

    Updating interaction_component.gd

--- Scripts/interaction_component.gd
+++ Scripts/interaction_component.gd
@@ -1,6 +1,9 @@
 class_name InteractionComponent extends Node
 
 @export var mesh : MeshInstance3D
+@export var context : String
+@export var override_icon : bool
+@export var new_icon : Texture2D
 
 var highlight_material = preload("res://Assets/InteractableHighlight.tres")
 
@@ -23,9 +26,12 @@ func connect_parent() -> void:
 
 func in_range() -> void:
 	mesh.material_overlay = highlight_material
+	Global.ui_context.update_content(context)
+	Global.ui_context.update_icon(new_icon, override_icon)
 
 func not_in_range() -> void:
 	mesh.material_overlay = null
+	Global.ui_context.reset()
 
 func on_interact() -> void:
 	print(parent.name)
